ec5b0b3f9e2d6ff0078dfdac44c9d965f556d403
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-02-22 01:27:41 +0100

    Added fontawesome and Counter component

---
+++ src/components/TaskCounter.jsx
@@ -0,0 +1,25 @@
+import "./TaskCounter.css";
+import { useState } from "react";
+
+const TaskCounter = (props) => {
+    const [count, setCount] = useState(0);
+
+    const increaseCount = () => {
+        setCount(count + 1);
+    };
+
+    const decreaseCount = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+    return (
+        <div className="task-counter">
+            <button className="task-counter__button" onClick={decreaseCount}><span className="fa-solid fa-minus"></span></button>
+            <div className="task-counter__count"><span>{count}</span></div>
+            <button className="task-counter__button" onClick={increaseCount}><span className="fa-solid fa-plus"></span></button>
+        </div>
+    )
+}
+
+export default TaskCounter;
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,14 +1,16 @@
 import "./TaskItem.css";
+import TaskCounter from "./TaskCounter";
 
 const TaskItem = (props) => {
     return (
         <div className="task-item">
             <div className="task-item__details">
+                <div className="task-item__emoji">&#128261;</div>
                 <h1 className="task-item__name">{props.data.name}</h1>
                 <p className="task-item__description">{props.data.description}</p>
             </div>
-            <div className="task-item-counter">
-
+            <div className="task-item__action">
+                <TaskCounter />
             </div>
         </div>
     )
