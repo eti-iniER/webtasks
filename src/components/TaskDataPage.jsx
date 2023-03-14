efa4691fe926436b8e755b665ca2243308f3a5f3
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-03-14 21:10:52 +0100

    Further implemented Stats page component

--- src/components/TaskDataPage.jsx
+++ src/components/TaskDataPage.jsx
@@ -9,13 +9,21 @@ const TaskDataPage = (props) => {
     return (
         <MenuModal isVisible={props.visible} close={closeSelf}>
             <div className="task-data-page">
-                <ul className="task-data-page__menu">
-                    <li className="task-data-page__menu-item active">Stats</li>
-                    <li className="task-data-page__menu-item">Notes</li>
-                    <li className="task-data-page__menu-item">History</li>
-                </ul>
-                <div className="task-data-header">
+                <div className="task-data-page__overview">
+                    <ul className="task-data-page__menu">
+                        <li className="task-data-page__menu-item active">Stats</li>
+                        <li className="task-data-page__menu-item">Notes</li>
+                        <li className="task-data-page__menu-item">History</li>
+                    </ul>
                     <h1>{props.task.name}</h1>
+                </div>
+                <div className="task-data-page__section-container">
+                    <div className="task-data-page__section">
+                        <div className="task-data-page__section-header">
+                            <span className="section-title-icon fa-solid fa-fire"></span>
+                            <h2 className="section-title">Streaks</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </MenuModal>
