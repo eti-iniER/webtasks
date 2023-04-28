348b6e5228cb5103457816095cae3b6680764bc1
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-04-28 13:38:02 +0100

    Fixed maximum task frequency problem

--- src/components/TaskConfigPage.jsx
+++ src/components/TaskConfigPage.jsx
@@ -496,8 +496,6 @@ const TaskConfigPage = (props) => {
             <label>FREQUENCY</label>
             <input
               type="number"
-              min={2}
-              max={6}
               value={taskOccurence}
               onChange={(e) => handleOccurenceInput(e)}
             ></input>
